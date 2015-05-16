935a632c75bc8c8a92846729eb35cefad325baed
Andrew Wittrock <andy@wittrock.us>
2015-05-16 14:26:56 -0500

    Finish initial implementation of contact form

--- src/ATPContact/Controller/IndexController.php
+++ src/ATPContact/Controller/IndexController.php
@@ -12,6 +12,8 @@ class IndexController extends \ATPCore\Controller\AbstractController
 		$view->fields = $field->loadMultiple(array(
 			'orderBy' => 'sort_order ASC'
 		));
+
+		$view->addChild(new \ATPCore\View\Widget\FlashWidget($this->flashMessenger()), 'flash');
 		
 		return $view;
 	}
@@ -30,6 +32,37 @@ class IndexController extends \ATPCore\Controller\AbstractController
 			$messageData[$field->label] = $this->params()->fromPost($name);
 		}
 		
-		echo "<pre>";print_r($messageData);die();
+		$messageHtml = $this->siteParam('contact-email-template');
+		foreach($messageData as $name => $value)
+		{
+			$messageHtml = str_replace('{' . $name . '}', $value, $messageHtml);
+		}
+		$messageText = strip_tags($messageHtml);
+
+		$replyToField = new \ATPContact\Model\Field();
+		$replyToField->loadById($this->siteParam('contact-reply-to-field'));
+		$mandrillMessage = array(
+			'html' => $messageHtml,
+			'text' => $messageText,
+			'subject' => $this->siteParam('contact-subject'),
+			'from_email' => $this->siteParam('contact-email-from'),
+			'to' => array(
+				array(
+					'email' => $this->siteParam('contact-email-to'),
+					'type' => 'to',
+				),
+			),
+			'headers' => array('Reply-To' => $messageData[$replyToField->label]),
+		);
+		
+		$mandrill = new \Mandrill($this->siteParam('mandrill-api-key'));
+		$mandrill->messages->send($mandrillMessage);
+		
+		//TODO: add confirmation message
+		$this->flash = $this->flashMessenger();
+		$this->flash->addSuccessMessage($this->siteParam('contact-success-message'));
+		
+		//TODO: forward back to contact page
+		$this->redirect()->toRoute('contact');
 	}
 }
